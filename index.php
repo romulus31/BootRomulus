a706189c0d72b199929180084c4a881810c04f82
Romain <Romain@192.168.1.36>
2015-03-08 19:49:30 +0100

    Adding some css

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>    …
+<script src="Framework/Bootstrap/js/jquery.js"></script>
+<script src="Framework/Bootstrap/js/bootstrap.js"></script>
+
+<link href="Framework/Bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>   
+    
+<body>
+<div class="container">
+<div class="row">
+  <div class="col-sm-4">Largeur 4</div>
+  <div class="col-sm-8">Largeur 8</div>
+</div>
+</div>    
+    
+    
+    
+    
+    
+</body>
+    
+    
+    
+    
+    
+    
+</html>
